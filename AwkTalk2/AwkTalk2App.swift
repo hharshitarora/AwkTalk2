a0e55a35d8d3d772d0dba0b7c5274aeba6d7a966
Harshit Arora <haroragoku@gmail.com>
2025-03-05 21:29:52 -0600

    Initial Commit

---
+++ AwkTalk2/AwkTalk2App.swift
@@ -0,0 +1,17 @@
+//
+//  AwkTalk2App.swift
+//  AwkTalk2
+//
+//  Created by Harshit Arora on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct AwkTalk2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AwkTalk2Tests/AwkTalk2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AwkTalk2Tests.swift
+//  AwkTalk2Tests
+//
+//  Created by Harshit Arora on 3/5/25.
+//
+
+import Testing
+@testable import AwkTalk2
+
+struct AwkTalk2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
